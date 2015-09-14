579486b1144ad45f979d41c21ec16ece05dc1393
Jamie Gabriel <jgab3103@gmail.com>
2015-09-14 16:26:58 +1000

    update

--- FinalDataSet/JSONFilesLoadedIntoDataBase/testScript.js
+++ FinalDataSet/JSONFilesLoadedIntoDataBase/testScript.js
@@ -1,2 +1,36 @@
-s1 = {"$group":{"_id":"all", "sum":{"$sum":1}}}
-db.corpusV2.aggregate(s1)
+query = 
+	{$group:
+  		{"_id":"all", "average":
+  		  {$avg: "$noteFrequency"
+  		  }
+  		}
+	}
+db.corpusV2.aggregate(query)
+
+query = 
+{ $group: 
+  { "_id": "all",
+    "sum" :{ $sum : 1},
+    "avg": { $avg: "$noteFrequency"},
+    "min": {$min: "$noteFrequency"},
+    "max": {$max: "$noteFrequency"},
+  }
+}
+db.corpusV2.aggregate(query)
+ 
+ 
+query = 
+{$group: {"_id":"$noteFrequency", "sum":{"$sum":1}}}
+
+query =
+{$group: {"_id":"$title", "val":{"$sum":1}}}
+db.corpusV2.aggregate(query)
+
+db.corpusV2.findOne()
+
+query = 
+{$group:{"_id":"$noteFrequency", 'Number of times this occurs':{"$sum":1}}}
+
+db.corpusV2.aggregate(query)
+
+
--- ls
+++
@@ -1,6 +0,0 @@
-Title,Year Recorded,Key Signature,Tempo of this perf,Time Signature,Length 
-Stella By Starlight,1983,Bb Major,220,4/4.,296
-Groovin High,1999,Eb Major,180,4/4.,189
-If I Were A Bell,2002,Ab Major,178,4/4.,187
-Autumn Leaves,1996,G minor,200,4/4.,200
-All The Things You Are,1983,Ab Major,120,4/4.,300
